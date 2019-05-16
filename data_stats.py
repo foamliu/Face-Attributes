ed34b14b674aa3c74abd9d0d164a180db6f9507a
Yang Liu <liuyang12@focusmedia.cn>
2019-05-16 15:11:38 +0800

    update

--- data_stats.py
+++ data_stats.py
@@ -7,30 +7,39 @@ from tqdm import tqdm
 
 from config import pickle_file
 
+
+def compute_distribution(name):
+    print('computing {}...'.format(name))
+
+    x = []
+    for sample in tqdm(samples):
+        value = sample['attr'][name]
+        x.append(value)
+
+    bins = np.linspace(0, 100, 101)
+
+    # the histogram of the data
+    plt.hist(x, bins, density=True, alpha=0.5, label='1', facecolor='blue')
+
+    mu = np.mean(x)
+    sigma = np.std(x)
+    y = scipy.stats.norm.pdf(bins, mu, sigma)
+    plt.plot(bins, y, 'r--')
+    plt.xlabel(name)
+    plt.ylabel('{} distribution'.format(name))
+    plt.title('Histogram: mu={:.4f}, sigma={:.4f}'.format(mu, sigma))
+
+    plt.savefig('images/{}_dist.png'.format(name))
+    plt.grid(True)
+    plt.show()
+
+
 if __name__ == "__main__":
     with open(pickle_file, 'rb') as file:
         data = pickle.load(file)
 
     samples = data['samples']
 
-    beauties = []
-    for sample in tqdm(samples):
-        beauty = sample['attr']['beauty']
-        beauties.append(beauty)
+    compute_distribution('age')
+    compute_distribution('beauty')
 
-    bins = np.linspace(0, 100, 101)
-
-    # the histogram of the data
-    plt.hist(beauties, bins, density=True, alpha=0.5, label='1', facecolor='blue')
-
-    mu = np.mean(beauties)
-    sigma = np.std(beauties)
-    y = scipy.stats.norm.pdf(bins, mu, sigma)
-    plt.plot(bins, y, 'b--')
-    plt.xlabel('beauty')
-    plt.ylabel('beauty distribution')
-    plt.title(r'Histogram: mu={:.4f},sigma={:.4f}'.format(mu, sigma))
-
-    plt.savefig('images/beauty_dist.png')
-    plt.grid(True)
-    plt.show()
