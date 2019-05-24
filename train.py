5cccbd52667cdc84d9a2cf15ac035e702c674fcb
Yang Liu <liuyang12@focusmedia.cn>
2019-05-24 11:31:17 +0800

    update

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import torch
 from tensorboardX import SummaryWriter
 from torch import nn
 from torch.optim.lr_scheduler import StepLR
+
 from config import device, grad_clip, print_freq, name_list, loss_ratio
 from data_gen import FaceAttributesDataset
 from models import FaceAttributesModel
@@ -48,9 +49,9 @@ def train_net(args):
 
     # Custom dataloaders
     train_dataset = FaceAttributesDataset('train')
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=2)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=4)
     valid_dataset = FaceAttributesDataset('valid')
-    valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=args.batch_size, shuffle=False, num_workers=2)
+    valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=args.batch_size, shuffle=False, num_workers=4)
 
     scheduler = StepLR(optimizer, step_size=args.lr_step, gamma=0.1)
 
