00581c9ecdccb5e18e4352295dd8f71f64f1ce52
Yang Liu <liuyang12@focusmedia.cn>
2019-05-31 10:39:40 +0800

    update

--- demo.py
+++ demo.py
@@ -74,43 +74,44 @@ if __name__ == "__main__":
     with torch.no_grad():
         output = model(inputs)
 
-    age_out, pitch_out, roll_out, yaw_out, beauty_out, expression_out, face_prob_out, face_shape_out, face_type_out, gender_out, glasses_out, race_out = output
+    # age_out, pitch_out, roll_out, yaw_out, beauty_out, expression_out, face_prob_out, face_shape_out, face_type_out, gender_out, glasses_out, race_out = output
+    beauty_out = output
 
-    _, expression_out = expression_out.topk(1, 1, True, True)
-    _, face_shape_out = face_shape_out.topk(1, 1, True, True)
-    _, face_type_out = face_type_out.topk(1, 1, True, True)
-    _, gender_out = gender_out.topk(1, 1, True, True)
-    _, glasses_out = glasses_out.topk(1, 1, True, True)
-    _, race_out = race_out.topk(1, 1, True, True)
-
-    age_out = age_out.cpu().numpy()
-    pitch_out = pitch_out.cpu().numpy()
-    roll_out = roll_out.cpu().numpy()
-    yaw_out = yaw_out.cpu().numpy()
+    # _, expression_out = expression_out.topk(1, 1, True, True)
+    # _, face_shape_out = face_shape_out.topk(1, 1, True, True)
+    # _, face_type_out = face_type_out.topk(1, 1, True, True)
+    # _, gender_out = gender_out.topk(1, 1, True, True)
+    # _, glasses_out = glasses_out.topk(1, 1, True, True)
+    # _, race_out = race_out.topk(1, 1, True, True)
+    #
+    # age_out = age_out.cpu().numpy()
+    # pitch_out = pitch_out.cpu().numpy()
+    # roll_out = roll_out.cpu().numpy()
+    # yaw_out = yaw_out.cpu().numpy()
     beauty_out = beauty_out.cpu().numpy()
-    expression_out = expression_out.cpu().numpy()
-    face_prob_out = face_prob_out.cpu().numpy()
-    face_shape_out = face_shape_out.cpu().numpy()
-    face_type_out = face_type_out.cpu().numpy()
-    gender_out = gender_out.cpu().numpy()
-    glasses_out = glasses_out.cpu().numpy()
-    race_out = race_out.cpu().numpy()
+    # expression_out = expression_out.cpu().numpy()
+    # face_prob_out = face_prob_out.cpu().numpy()
+    # face_shape_out = face_shape_out.cpu().numpy()
+    # face_type_out = face_type_out.cpu().numpy()
+    # gender_out = gender_out.cpu().numpy()
+    # glasses_out = glasses_out.cpu().numpy()
+    # race_out = race_out.cpu().numpy()
 
     for i in range(10):
         sample = sample_preds[i]
 
-        sample['age_out'] = int(age_out[i][0] * 100)
-        sample['pitch_out'] = float('{0:.2f}'.format(pitch_out[i][0] * 360 - 180))
-        sample['roll_out'] = float('{0:.2f}'.format(roll_out[i][0] * 360 - 180))
-        sample['yaw_out'] = float('{0:.2f}'.format(yaw_out[i][0] * 360 - 180))
+        # sample['age_out'] = int(age_out[i][0] * 100)
+        # sample['pitch_out'] = float('{0:.2f}'.format(pitch_out[i][0] * 360 - 180))
+        # sample['roll_out'] = float('{0:.2f}'.format(roll_out[i][0] * 360 - 180))
+        # sample['yaw_out'] = float('{0:.2f}'.format(yaw_out[i][0] * 360 - 180))
         sample['beauty_out'] = float('{0:.2f}'.format(beauty_out[i][0] * 100))
-        sample['expression_out'] = idx2name(int(expression_out[i][0]), 'expression')
-        sample['face_prob_out'] = float('{0:.4f}'.format(face_prob_out[i][0]))
-        sample['face_shape_out'] = idx2name(int(face_shape_out[i][0]), 'face_shape')
-        sample['face_type_out'] = idx2name(int(face_type_out[i][0]), 'face_type')
-        sample['gender_out'] = idx2name(int(gender_out[i][0]), 'gender')
-        sample['glasses_out'] = idx2name(int(glasses_out[i][0]), 'glasses')
-        sample['race_out'] = idx2name(int(race_out[i][0]), 'race')
+        # sample['expression_out'] = idx2name(int(expression_out[i][0]), 'expression')
+        # sample['face_prob_out'] = float('{0:.4f}'.format(face_prob_out[i][0]))
+        # sample['face_shape_out'] = idx2name(int(face_shape_out[i][0]), 'face_shape')
+        # sample['face_type_out'] = idx2name(int(face_type_out[i][0]), 'face_type')
+        # sample['gender_out'] = idx2name(int(gender_out[i][0]), 'gender')
+        # sample['glasses_out'] = idx2name(int(glasses_out[i][0]), 'glasses')
+        # sample['race_out'] = idx2name(int(race_out[i][0]), 'race')
 
     with open('sample_preds.json', 'w') as file:
         json.dump(sample_preds, file, indent=4, ensure_ascii=False)
