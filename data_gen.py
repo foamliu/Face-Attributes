fe56cb6facd06aa216f3edc302b1418aefc97f51
Yang Liu <liuyang12@focusmedia.cn>
2019-06-12 17:39:56 +0800

    update

--- data_gen.py
+++ data_gen.py
@@ -12,7 +12,7 @@ from utils import crop_image
 # Just normalization for validation
 data_transforms = {
     'train': transforms.Compose([
-        transforms.RandomHorizontalFlip(),
+        # transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
     ]),
