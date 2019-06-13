0deb2eda76a14ded7a324ce75faa0223d1931dea
Yang Liu <liuyang12@focusmedia.cn>
2019-06-13 18:19:13 +0800

    update

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import numpy as np
 import torch
 from tensorboardX import SummaryWriter
 from torch import nn
-
+from torch.optim.lr_scheduler import StepLR
 from config import device, grad_clip, print_freq, loss_ratio
 from data_gen import FaceAttributesDataset
 from models import FaceAttributeModel
@@ -52,10 +52,12 @@ def train_net(args):
     valid_dataset = FaceAttributesDataset('valid')
     valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=args.batch_size, shuffle=False, num_workers=4)
 
-    # scheduler = StepLR(optimizer, step_size=args.lr_step, gamma=0.1)
+    scheduler = StepLR(optimizer, step_size=args.lr_step, gamma=0.1)
 
     # Epochs
     for epoch in range(start_epoch, args.end_epoch):
+        scheduler.step(epoch)
+
         # One epoch's training
         train_loss = train(train_loader=train_loader,
                            model=model,
